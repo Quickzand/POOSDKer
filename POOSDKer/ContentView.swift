b5bb49f5701c1ab798d10e371c13547105464963
Matthew Sand <44583344+Quickzand@users.noreply.github.com>
2024-02-17 17:27:35 -0500

    Initial Commit

---
+++ POOSDKer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  POOSDKer
+//
+//  Created by Matthew Sand on 2/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ POOSDKer/POOSDKerApp.swift
@@ -0,0 +1,17 @@
+//
+//  POOSDKerApp.swift
+//  POOSDKer
+//
+//  Created by Matthew Sand on 2/17/24.
+//
+
+import SwiftUI
+
+@main
+struct POOSDKerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
