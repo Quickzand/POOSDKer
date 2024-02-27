067d2e0be84494a9ee3e4f833b2a0598cf30adbc
Matthew Sand <44583344+Quickzand@users.noreply.github.com>
2024-02-27 13:42:42 -0500

    Added wireframe views and navigation

---
+++ POOSDKer/Views/PlayGameView.swift
@@ -0,0 +1,20 @@
+//
+//  PlayGameView.swift
+//  POOSDKer
+//
+//  Created by Matthew Sand on 2/27/24.
+//
+
+import SwiftUI
+
+struct PlayGameView: View {
+    var body: some View {
+        VStack {
+            Text("Play Game")
+        }
+    }
+}
+
+#Preview {
+    PlayGameView()
+}
---
+++ POOSDKer/Views/ResultsView.swift
@@ -0,0 +1,20 @@
+//
+//  ResultsView.swift
+//  POOSDKer
+//
+//  Created by Matthew Sand on 2/27/24.
+//
+
+import SwiftUI
+
+struct ResultsView: View {
+    var body: some View {
+        VStack {
+            Text("Results")
+        }
+    }
+}
+
+#Preview {
+    ResultsView()
+}
